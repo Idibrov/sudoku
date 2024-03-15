c959c54a9fe2ad4ee5d13059629e861bfbabc48e
0GI4 <pop9991@outlook.com>
2024-03-15 13:40:14 +0300

    del shit

--- sudoku.js
+++ sudoku.js
@@ -1,35 +1,6 @@
 function read() {
-  /**
-   * Enumerating objects: 14, done.
-Counting objects: 100% (14/14), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (11/11), done.
-Writing objects: 100% (14/14), 51.84 KiB | 965.00 KiB/s, done.
-Total 14 (delta 0), reused 0 (delta 0), pack-reused 0
-To github.com:Idibrov/sudoku.gitEnumerating objects: 14, done.
-Counting objects: 100% (14/14), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (11/11), done.
-Writing objects: 100% (14/14), 51.84 KiB | 965.00 KiB/s, done.
-Total 14 (delta 0), reused 0 (delta 0), pack-reused 0
-To github.com:Idibrov/sudoku.git
- * [new branch]      main -> mainEnumerating objects: 14, done.
-Counting objects: 100% (14/14), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (11/11), done.
-Writing objects: 100% (14/14), 51.84 KiB | 965.00 KiB/s, done.
-Total 14 (delta 0), reused 0 (delta 0), pack-reused 0
-To github.com:Idibrov/sudoku.git
- * [new branch]      main -> mainEnumerating objects: 14, done.
-Counting objects: 100% (14/14), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (11/11), done.
-Writing objects: 100% (14/14), 51.84 KiB | 965.00 KiB/s, done.
-Total 14 (delta 0), reused 0 (delta 0), pack-reused 0
-To github.com:Idibrov/sudoku.git
- * [new branch]      main -> main
- * [new branch]      main -> mainПрочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
-   */
+//  Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
+//   
 }
 
 function solve() {
@@ -37,7 +8,6 @@ function solve() {
    * Принимает игровое поле в том формате, в котором его вернули из функции read.
    * Возвращает игровое поле после попытки его решить.
    */
-  return 2+2
 }
 
 function isSolved() {
